421d0def84bc12667ef5efb8962c46928488588c
unifiedhealthtech-dev <unifiedhealthtech@gmail.com>
2026-02-10 22:23:40 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/Pharmazen-Pro/",
+  base: "/Pharmazen-Website/",
   server: {
     host: "::",
     port: 8080,
